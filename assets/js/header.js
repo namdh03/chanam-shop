a3311fe3a4ce4c4141c813049664511fb2bb90a1
hachang24 <hthhuyntrang@gmail.com>
2023-01-13 08:39:46 +0700

    fix branch chang

--- assets/js/header.js
+++ assets/js/header.js
@@ -29,22 +29,22 @@ export default function header() {
     });
 
     // MENU MOBILE
-    let section = document.querySelectorAll('.sid-right');
-    let navLinks = document.querySelectorAll('.navbar__menu-items')
+    // let section = document.querySelectorAll('.sid-right');
+    // let navLinks = document.querySelectorAll('.navbar__menu-items')
 
-    window.onscroll = () => {
-        section.forEach(sec => {
-            let top = window.scrollY;
-            let offset = sec.offsetTop;
-            let height = sec.offsetHeight;
-            let id = sec.getAttribute('id');
+    // window.onscroll = () => {
+    //     section.forEach(sec => {
+    //         let top = window.scrollY;
+    //         let offset = sec.offsetTop;
+    //         let height = sec.offsetHeight;
+    //         let id = sec.getAttribute('id');
 
-            if (top >= offset && top < offset + height) {
-                navLinks.forEach(links => {
-                    links.classList.remove("active")
-                    document.querySelector('.navbar__menu--items[href*=' + id + ']').classList.add("active");
-                });
-            }
-        });
-    }
+    //         if (top >= offset && top < offset + height) {
+    //             navLinks.forEach(links => {
+    //                 links.classList.remove("active")
+    //                 document.querySelector('.navbar__menu--items[href*=' + id + ']').classList.add("active");
+    //             });
+    //         }
+    //     });
+    // }
 }
